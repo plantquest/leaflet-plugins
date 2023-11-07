19867ef7db1e419a5234896d7c33633715292415
Ríona Stokes <rionastokes@gmail.com>
2023-11-07 21:55:41 +0000

    Set up basic Geofence class and constructor, view in console

--- GeofenceDisplay/src/geofence-display.ts
+++ GeofenceDisplay/src/geofence-display.ts
@@ -2,12 +2,10 @@ import L from 'leaflet'
 import './geofence-display.css'
 
 const PlantquestGeofenceDisplay = L.Control.extend({
-  options: {
-    position: 'topright',
-  },
+  options: {},
 
   initialize: function (this: any, options: any) {
-    console.log('PGS init')
+    console.log('PGeoD init')
     this._state = {
       open: false,
       atype: {},
@@ -18,104 +16,31 @@ const PlantquestGeofenceDisplay = L.Control.extend({
   },
 
   onAdd: function (this: any, _map: any) {
-    let self = this
-
-    console.log('PGS onAdd', this.options)
+    console.log('PGeoD onAdd', this.options)
     let div = L.DomUtil.create('div')
-    div.classList.add('pqs-map-control-group')
 
-    let activate = L.DomUtil.create('div')
-    activate.classList.add('pqs-map-control-group-activate')
-    // activate.innerHTML = '<i aria-hidden="true" class="v-icon notranslate vxg-icon mdi mdi-map-search-outline theme--light" style="font-size: 24px; padding: 8px; color: black;"></i>'
-    activate.innerHTML = '<div style="color:red;"><b>GS</b></div>'
-    activate.addEventListener('click', () => {
-      this._state.open = !this._state.open
-
-      if (this._state.open) {
-        div.appendChild(this._menu)
-      } else {
-        div.removeChild(this._menu)
-      }
-    })
-
-    let menu = (this._menu = L.DomUtil.create('div'))
-    menu.classList.add('pqs-map-control-group-menu')
-
-    let ul = L.DomUtil.create('ul')
-
-    let li = L.DomUtil.create('li')
-    li.classList.add('pqs-map-control-group-menu-head')
-    li.innerText = 'ROOM/AREA ATTRIBUTES'
-    ul.appendChild(li)
-
-    for (let attrField in this.options.room.attr) {
-      li = L.DomUtil.create('li')
-      li.innerText = this.options.room.attr[attrField]
-      li.addEventListener(
-        'click',
-        ((attrField, li) => () => {
-          self.options.toggleGroup('roomattr', attrField)
-          self._state.roomattr[attrField] = !self._state.roomattr[attrField]
-          if (self._state.roomattr[attrField]) {
-            li.classList.add('pqs-map-control-group-active')
-          } else {
-            li.classList.remove('pqs-map-control-group-active')
-          }
-        })(attrField, li)
-      )
-      ul.appendChild(li)
-      this._state.menuitems.push(li)
-    }
-
-    li = L.DomUtil.create('li')
-    li.classList.add('pqs-map-control-group-menu-head')
-    li.innerText = 'POINTS OF INTEREST'
-    ul.appendChild(li)
-
-    for (let atype of this.options.asset.atypes) {
-      li = L.DomUtil.create('li')
-      li.innerText = atype
-      li.addEventListener(
-        'click',
-        ((atype, li) => () => {
-          self.options.toggleGroup('asset', atype)
-          self._state.atype[atype] = !self._state.atype[atype]
-          if (self._state.atype[atype]) {
-            li.classList.add('pqs-map-control-group-active')
-          } else {
-            li.classList.remove('pqs-map-control-group-active')
-          }
-        })(atype, li)
-      )
-      ul.appendChild(li)
-      this._state.menuitems.push(li)
-    }
-
-    menu.appendChild(ul)
-    // menu.innerText = 'MENU'
-
-    div.appendChild(activate)
-
+    let geo = new Geofence(null, null)
+    console.log('geo:', geo)
     return div
   },
 
   onRemove: function (this: any, _map: any) {
-    console.log('PGS onRemove')
+    console.log('PGeoD onRemove')
   },
 
   clear: function (this: any) {
-    for (let p in this._state.atype) {
-      this._state.atype[p] = false
-    }
-
-    for (let p in this._state.roomattr) {
-      this._state.roomattr[p] = false
-    }
-
-    this._state.menuitems.map((li: any) =>
-      li.classList.remove('pqs-map-control-group-active')
-    )
+    console.log('PGeoD clear')
   },
 })
+
+class Geofence {
+  ent = null
+  ctx = null
+
+  constructor(ent: any, ctx: any) {
+    this.ent = ent
+    this.ctx = ctx
+  }
+}
 
 export { PlantquestGeofenceDisplay }
