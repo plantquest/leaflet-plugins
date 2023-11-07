0a2b15cdf86c941b68adfb313fe9662c778e6c3d
Ríona Stokes <rionastokes@gmail.com>
2023-11-07 23:15:45 +0000

    All geofence code extracted from main file (excl seneca msg)

--- GeofenceDisplay/src/geofence-display.ts
+++ GeofenceDisplay/src/geofence-display.ts
@@ -6,12 +6,7 @@ const PlantquestGeofenceDisplay = L.Control.extend({
 
   initialize: function (this: any, options: any) {
     console.log('PGeoD init')
-    this._state = {
-      open: false,
-      atype: {},
-      roomattr: {},
-      menuitems: [],
-    }
+    this._state = {}
     L.Util.setOptions(this, options)
   },
 
@@ -19,8 +14,91 @@ const PlantquestGeofenceDisplay = L.Control.extend({
     console.log('PGeoD onAdd', this.options)
     let div = L.DomUtil.create('div')
 
-    let geo = new Geofence(null, null)
-    console.log('geo:', geo)
+    // let geo = new Geofence(null, null)
+    // console.log('geo:', geo)
+
+    self.clearGeofences()
+
+    //
+
+    // Load entities.
+    const entnames = ['building', 'level', 'room', 'geofence', 'asset']
+
+    // Clear previous data.
+    for (let kind of entnames) {
+      self.data[kind] = self.data[kind] || []
+      self.data[kind].length = 0
+    }
+
+    self.data.geofence.forEach((ent) => {
+      let geofence = new Geofence(ent, ctx)
+      self.geofence.map[ent.id] =
+        self.config.geofence.prepare(geofence) || geofence
+    })
+
+    //
+
+    loadData()
+
+    //
+
+    self.map.createPane('geofence')
+    let geofencePane = self.map.getPane('geofence')
+    geofencePane.style.zIndex = 230
+    geofencePane.style.pointerEvents = 'none'
+
+    self.map.createPane('geofenceLabel')
+    let geofenceLabelPane = self.map.getPane('geofenceLabel')
+    geofenceLabelPane.style.zIndex = 235
+    geofenceLabelPane.style.pointerEvents = 'none'
+
+    self.layer.geofence = L.layerGroup(null, { pane: 'geofence' }).addTo(
+      self.map
+    )
+    self.layer.geofence.name$ = 'geofence'
+
+    //
+
+    self.showGeofence = function (geofence, show) {
+      if (null == geofence) {
+        return
+      }
+
+      show = !!show
+
+      if (true === show) {
+        geofence.show(self.layer.geofence)
+      } else if (false === show) {
+        geofence.hide()
+      }
+    }
+
+    self.clearGeofences = function () {
+      for (let geofenceID in self.geofence.map) {
+        let geofence = self.geofence.map[geofenceID]
+        delete self.geofence.map[geofenceID]
+        if (geofence && geofence.hide) {
+          geofence.hide()
+        }
+      }
+    }
+
+    //
+
+    if (self.config.geofence.show.all) {
+      self.send({
+        srv: 'plantquest',
+        part: 'assetmap',
+        show: 'geofence',
+        geofence: null,
+      })
+    }
+
+    //
+
+    let ents = ['asset', 'room', 'building', 'level', 'geofence']
+
+    //
     return div
   },
 
