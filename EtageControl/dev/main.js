dfef23d7566463c84fabc39da98df7dbda53b855
Jose Perigolo <neto.perigolo@gmail.com>
2023-11-08 06:37:13 -0300

    Update main.js

--- EtageControl/dev/main.js
+++ EtageControl/dev/main.js
@@ -6,8 +6,6 @@ import L from 'leaflet'
 import '@plantquest/etage-control/dist/etage-control.css'
 import { PlantquestEtageControl } from '@plantquest/etage-control'
 
-console.log('START', PlantquestEtageControl)
-
 
 const map = L.map('map').setView([51.505, -0.09], 13)
 
@@ -16,30 +14,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
-
-const plantquestEtageControl = new PlantquestEtageControl({
+// In progress:
+// Just some mock-up options for buildings and floors, testing it visually.
+const etageControlOptions = {
   position: 'topright',
-  asset: {
-    atypes: [
-      'Emergency Exit',
-      'De-Fibrilator',
-      'Muster Point',
-    ]
-  },
-  room: {
-    attr: {
-      data8:  'Meeting Room',
-      data9:  'Plant Room',   				
-      data10: 'ATEX Area',
-      data11: 'Noise >80db',
-      data12: 'Production Area',
-      data13: 'PPE Required',
-    }
-  },
-  toggleGroup: (kind, value)=>{
-    console.log('toggleGroup', kind, value)
-  }
-})
+  buildings: [
+    { name: 'Building A', center: [51.505, -0.09], zoom: 18 },
+    { name: 'Building B', center: [51.510, -0.11], zoom: 18 },
+    // ... more buildings
+  ],
+  floors: [
+    { name: 'Floor 1', data: 'Data for Floor 1' },
+    { name: 'Floor 2', data: 'Data for Floor 2' },
+    // ... more floors
+  ]
+};
+
+
+const plantquestEtageControl = new PlantquestEtageControl(etageControlOptions)
 
 plantquestEtageControl.addTo(map)
 
