b9e495cc4e34951d596154109e584b150f23a8d6
Ríona Stokes <rionastokes@gmail.com>
2023-11-13 19:02:48 +0000

    Requested changes from PR

--- GeofenceDisplay/src/geofence-display.ts
+++ GeofenceDisplay/src/geofence-display.ts
@@ -1,12 +1,27 @@
-export {}
-
 import L from 'leaflet'
+// import Gubu from 'gubu'
 import './geofence-display.css'
-import './rastercoords.js'
+import { RasterCoords } from './rastercoords'
+
+// TODO: implement Gubu with interfaces
+interface Geofence {
+  id: string
+  title: string
+  latlngs: Array<Array<string>>
+  colour: string
+}
+
+interface PlantquestGeofenceDisplayOptions extends L.ControlOptions {
+  debug: boolean
+  geofences: Geofence[]
+}
 
 const PlantquestGeofenceDisplay = L.Layer.extend({
+  // Use GUBU for options validation
   initialize: function (this: any, options: any) {
-    console.log('GeofenceDisplay init function called.')
+    let validatedOptions = options as PlantquestGeofenceDisplayOptions
+    validatedOptions.debug &&
+      console.log('GeofenceDisplay init function called.')
     L.Util.setOptions(this, options)
     this._state = {
       zindex: 0,
@@ -15,33 +30,44 @@ const PlantquestGeofenceDisplay = L.Layer.extend({
   },
 
   onAdd: function (this: any, _map: any) {
-    console.log('GeofenceDisplay onAdd function called.')
     let self = this
-    console.log('Local geofence variable before add:', self._state.geofenceByID)
-    for (let geodata in self.options.geofences) {
-      let geo = self.options.geofences[geodata]
-      let geofence = new Geofence(geo, null)
+    self.options.debug && console.log('GeofenceDisplay onAdd function called.')
+    self.options.debug &&
+      console.log(
+        'Local geofence variable before add:',
+        self._state.geofenceByID
+      )
+    self.options.geofences.forEach((fence: any) => {
+      let geo = self.options.geofences[fence]
+      let geofence = new Geofence(geo, { map: _map })
       self._state.geofenceByID[geo.id] = geofence
-      geofence.show(_map)
-    }
-    console.log('Local geofence variable after add:', self._state.geofenceByID)
+      geofence.show()
+    })
+    self.options.debug &&
+      console.log(
+        'Local geofence variable after add:',
+        self._state.geofenceByID
+      )
   },
 
   onRemove: function (this: any, _map: any) {
-    console.log('GeofenceDisplay onRemove function called.')
     let self = this
-    console.log(
-      'Local geofence variable before remove:',
-      self._state.geofenceByID
-    )
+    self.options.debug &&
+      console.log('GeofenceDisplay onRemove function called.')
+    self.options.debug &&
+      console.log(
+        'Local geofence variable before remove:',
+        self._state.geofenceByID
+      )
     for (let id in self._state.geofenceByID) {
       self._state.geofenceByID[id].hide()
       delete self._state.geofenceByID[id]
     }
-    console.log(
-      'Local geofence variable after remove:',
-      self._state.geofenceByID
-    )
+    self.options.debug &&
+      console.log(
+        'Local geofence variable after remove:',
+        self._state.geofenceByID
+      )
   },
 })
 
@@ -55,30 +81,31 @@ class Geofence {
     this.ctx = ctx
   }
 
-  show(map: any) {
-    if (null == this.poly) {
-      let polyCoords = this.ent.latlngs
-      this.poly = L.polygon(polyCoords, {
-        color: this.ent.colour,
+  show() {
+    let self = this
+    if (null == self.poly) {
+      let polyCoords = self.ent.latlngs
+      self.poly = L.polygon(polyCoords, {
+        color: self.ent.colour,
       })
     }
-    this.poly.addTo(map)
+    self.poly.addTo(self.ctx.map)
   }
 
   hide() {
-    console.log('Geofence class hide function called.')
-    this.poly && this.poly.remove()
+    let self = this
+    self.poly && self.poly.remove()
   }
 
-  // convertPoly(img: any, poly: any) {
-  //   // Quote from docs: 'unproject `coords` to the raster coordinates used by the raster image projection'
-  //   let p = []
-  //   let rc = new L.RasterCoords(self.map, self.config.mapImg)
-  //   for (let part of poly) {
-  //     p.push(rc.unproject([part[1], part[0]]))
+  //   convertPoly(img: any, poly: any) {
+  //     // Quote from docs: 'unproject `coords` to the raster coordinates used by the raster image projection'
+  //     let p = []
+  //     let rc = new RasterCoords(self.map, self.config.mapImg)
+  //     for (let part of poly) {
+  //       p.push(rc.unproject([part[1], part[0]]))
+  //     }
+  //     return p
   //   }
-  //   return p
-  // }
 }
 
 export { PlantquestGeofenceDisplay }
