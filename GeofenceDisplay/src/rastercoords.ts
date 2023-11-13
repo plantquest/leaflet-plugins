60f7b5e43e9feb27ea42b1f3df0607e69f123f07
Ríona Stokes <rionastokes@gmail.com>
2023-11-13 19:00:17 +0000

    Convert rastercoords.js to TypeScript

--- GeofenceDisplay/src/rastercoords.ts
+++ GeofenceDisplay/src/rastercoords.ts
@@ -1,3 +1,5 @@
+import * as L from 'leaflet'
+
 /**
  * leaflet plugin for plain image map projection
  * @copyright 2016- commenthol
@@ -6,55 +8,80 @@
 /* globals define */
 /* eslint no-var:off */
 
-L.RasterCoords = function (map, imgsize, tilesize, setmaxbounds = true) {
-  this.map = map
-  this.width = imgsize[0]
-  this.height = imgsize[1]
-  this.tilesize = tilesize || 256
-  this.zoom = this.zoomLevel()
-  if (setmaxbounds && this.width && this.height) {
-    this.setMaxBounds()
-  }
+interface RasterCoordsOptions {
+  map: any
+  imgsize: any
+  tilesize?: any
+  setmaxbounds?: any
 }
 
-L.RasterCoords.prototype = {
+class RasterCoords {
+  map: any
+  width: any
+  height: any
+  tilesize: any
+  zoom: any
+
+  constructor({
+    map,
+    imgsize,
+    tilesize = 256,
+    setmaxbounds = true,
+  }: RasterCoordsOptions) {
+    this.map = map
+    this.width = imgsize[0]
+    this.height = imgsize[1]
+    this.tilesize = tilesize
+    this.zoom = this.zoomLevel()
+
+    if (setmaxbounds && this.width && this.height) {
+      this.setMaxBounds()
+    }
+  }
+
   /**
    * calculate accurate zoom level for the given image size
    */
-  zoomLevel: function () {
+  private zoomLevel(): any {
     return Math.ceil(
       Math.log(Math.max(this.width, this.height) / this.tilesize) / Math.log(2)
     )
-  },
+  }
+
   /**
    * unproject `coords` to the raster coordinates used by the raster image projection
    * @param {Array} coords - [ x, y ]
    * @return {L.LatLng} - internal coordinates
    */
-  unproject: function (coords) {
+  unproject(coords: any): L.LatLng {
     return this.map.unproject(coords, this.zoom)
-  },
+  }
+
   /**
    * project `coords` back to image coordinates
    * @param {Array} coords - [ x, y ]
    * @return {L.LatLng} - image coordinates
    */
-  project: function (coords) {
+  project(coords: any): L.LatLng {
     return this.map.project(coords, this.zoom)
-  },
+  }
+
   /**
    * get the max bounds of the image
    */
-  getMaxBounds: function () {
-    var southWest = this.unproject([0, this.height])
-    var northEast = this.unproject([this.width, 0])
+  getMaxBounds(): L.LatLngBounds {
+    const southWest = this.unproject([0, this.height])
+    const northEast = this.unproject([this.width, 0])
     return new L.LatLngBounds(southWest, northEast)
-  },
+  }
+
   /**
    * sets the max bounds on map
    */
-  setMaxBounds: function () {
-    var bounds = this.getMaxBounds()
+  setMaxBounds(): void {
+    const bounds = this.getMaxBounds()
     this.map.setMaxBounds(bounds)
-  },
-} // eslint-disable-line semi
+  }
+}
+
+export { RasterCoords }
