52feb4380621d31f701db58837f302a57815561f
Ríona Stokes <rionastokes@gmail.com>
2023-11-21 13:19:43 +0000

    Stripped out visual aspect

--- Utility/src/utility.ts
+++ Utility/src/utility.ts
@@ -2,145 +2,31 @@ import L from 'leaflet'
 import { Gubu } from 'gubu'
 import './utility.css'
 
-// TODO: implement Gubu with interfaces
-interface GeofenceDef {
-  id: string
-  title: string
-  latlngs: Array<Array<string>>
-  colour: string
-}
+// interface UtilityDef {}
 
 interface PlantquestUtilityOptions extends L.ControlOptions {
   debug: boolean
-  geofences: GeofenceDef[]
 }
 
-const PlantquestUtility = L.Layer.extend({
+const PlantquestUtility = L.Control.extend({
   initialize: function (this: any, rawOptions: PlantquestUtilityOptions) {
     const PlantquestUtilityOptionsShape = Gubu({})
     let options = PlantquestUtilityOptionsShape(rawOptions)
     options.debug && console.log('Utility init function called.')
 
     L.Util.setOptions(this, options)
-    this._state = {
-      zindex: 0,
-      geofenceByID: {},
-    }
+    this._state = {}
   },
 
   onAdd: function (this: any, _map: any) {
     let self = this
     self.options.debug && console.log('Utility onAdd function called.')
-    self.options.debug &&
-      console.log(
-        'Local geofence variable before add:',
-        self._state.geofenceByID
-      )
-
-    self.options.geofences.forEach((geo: any) => {
-      let geofence = new Geofence(geo, {
-        map: _map,
-        cfg: { geofence: { click: { active: true } } },
-      })
-      self._state.geofenceByID[geo.id] = geofence
-      self.showGeofence(geofence, true)
-    })
-
-    self.options.debug &&
-      console.log(
-        'Local geofence variable after add:',
-        self._state.geofenceByID
-      )
   },
 
   onRemove: function (this: any, _map: any) {
     let self = this
     self.options.debug && console.log('Utility onRemove function called.')
-    self.options.debug &&
-      console.log(
-        'Local geofence variable before remove:',
-        self._state.geofenceByID
-      )
-
-    self.clearGeofences()
-
-    self.options.debug &&
-      console.log(
-        'Local geofence variable after remove:',
-        self._state.geofenceByID
-      )
-  },
-
-  showGeofence: function (geofence: Geofence, show: boolean) {
-    if (null == geofence) {
-      return
-    }
-    show = !!show
-    if (true === show) {
-      geofence.show()
-    } else if (false === show) {
-      geofence.hide()
-    }
-  },
-
-  clearGeofences: function () {
-    let self = this
-    for (let geofenceID in self._state.geofenceByID) {
-      let geofence = self._state.geofenceByID[geofenceID]
-      delete self._state.geofenceByID[geofenceID]
-      if (geofence && geofence.hide) {
-        geofence.hide()
-      }
-    }
   },
 })
-
-class Geofence {
-  ent: any = null
-  ctx: any = null
-  poly: ReturnType<typeof L.polygon>
-
-  constructor(ent: any, ctx: any) {
-    this.ent = ent
-    this.ctx = ctx
-    this.poly = L.polygon(ent.latlngs, { pane: ent.pane, color: ent.colour })
-  }
-
-  show() {
-    let self = this
-
-    if (self.ctx.cfg.geofence.click.active) {
-      self.poly.on('click', self.onClick.bind(self))
-    }
-
-    let tooltip = L.tooltip({
-      pane: 'geofenceLabel',
-      permanent: true,
-      direction: 'center',
-      opacity: 1,
-      className: 'polygon-labels',
-    })
-
-    self.poly.bindTooltip(tooltip)
-
-    tooltip.setContent(
-      '<div class="' +
-        'leaflet-zoom-animated ' +
-        'plantquest-geofence-label ' +
-        `">${self.ent.title}</div>`
-    )
-
-    self.poly.addTo(self.ctx.map)
-  }
-
-  hide() {
-    let self = this
-    self.poly && self.poly.remove()
-  }
-
-  onClick(event: any) {
-    console.log('onClick', event)
-  }
-}
 
 export { PlantquestUtility }
