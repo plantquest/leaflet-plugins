b2808f7ca96d63bf8ee10619d89f6531ca2fa0f7
Ríona Stokes <rionastokes@gmail.com>
2023-11-24 21:33:44 +0000

    Add AWS tiles as tileLayer to Geofence demo + initial test

--- GeofenceDisplay/dev/main.js
+++ GeofenceDisplay/dev/main.js
@@ -8,13 +8,47 @@ import { PlantquestGeofenceDisplay } from '@plantquest/geofence-display'
 
 console.log('START', PlantquestGeofenceDisplay)
 
-const map = L.map('map').setView([53.27, -9.055], 16)
+// const map = L.map('map').setView([53.27, -9.055], 16)
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map)
+//
+
+const map = L.map('plantquest-assetmap-map', {
+  crs: L.CRS.Simple,
+  scrollWheelZoom: false,
+  doubleClickZoom: false,
+  attributionControl: false,
+  // minZoom: self.config.mapMinZoom,
+  // maxZoom: self.config.mapMaxZoom,
+  editable: true,
+})
+
+// rc = self.rc = new L.RasterCoords(self.map, self.config.mapImg)
+// self.map.setMaxBounds(self.rc.getMaxBounds())
+
+// self.leaflet.maptile = self.createTile(mapIndex+1)
+// self.leaflet.maptile.addTo(self.map)
+
+// self.createTile = function(mapIndex) {
+// let tileLyr =
+//   L.tileLayer('https://plantquest-demo01-map01.s3.eu-west-1.amazonaws.com/tiles/pqd-pq01-m01-013', {
+//     bounds: self.rc.getMaxBounds(),
+//     minZoom: self.config.mapMinZoom,
+//     maxZoom: self.config.mapMaxZoom,
+//   })
+// return tileLyr
+// },
+
+//
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map)
+
+L.tileLayer(
+  'https://plantquest-demo01-map01.s3.eu-west-1.amazonaws.com/tiles/pqd-pq01-m01-013'
+).addTo(map)
 
 map.createPane('geofence')
 let geofencePane = map.getPane('geofence')
