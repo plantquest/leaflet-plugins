bcd0af9765a84a431a79a9a8ad3e949ad7637a20
Jose Perigolo <neto.perigolo@gmail.com>
2023-11-27 06:25:51 -0300

    Update main.js

--- DataLoader/dev/main.js
+++ DataLoader/dev/main.js
@@ -2,8 +2,6 @@ import { DataLoader } from '@plantquest/data-loader';
 // importing seneca for testing, as we have to receive/passthrough seneca instance
 import Seneca from 'seneca-browser'
 
-
-const map = L.map('map').setView([51.505, -0.09], 13);
 const dataLoader = new DataLoader({
   query: {
     project_id: 'project_id',
@@ -14,7 +12,6 @@ const dataLoader = new DataLoader({
   entityNames: ['building', 'level', 'room', 'geofence', 'asset']
 });
 
-dataLoader.addTo(map);
 
 // Load data
 dataLoader.loadData().then(data => {
